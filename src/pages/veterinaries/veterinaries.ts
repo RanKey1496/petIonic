0d6fc6ef2cad8835cda6043ddc3a375ac7e2c059
Jhon Gil Sepulveda <jhongil96@gmail.com>
2017-10-11 00:19:29 -0500

    Primera versión de las veterinarias

--- src/pages/veterinaries/veterinaries.ts
+++ src/pages/veterinaries/veterinaries.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the VeterinariesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-veterinaries',
@@ -15,7 +8,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class VeterinariesPage {
 
+  vets: Array<{title: string, dir: string, distance: number}>
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.vets = [
+      { title: "Carmenchis SA", dir: "Calle 30a #75-34", distance: 0.4 },
+      { title: "Peluditos Lindos", dir: "Carrera 75 #32-38", distance: 1 },
+      { title: "Gatico Cariñosito", dir: "Calle 33 #25-14", distance: 1.3 },
+      { title: "Clinica Veterinaria Aguapanela", dir: "Calle 80 #79-84", distance: 4 },
+    ]
   }
 
   ionViewDidLoad() {
