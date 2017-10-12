fb59451e43a5345b7335f561002a14bb3b397279
Jhon Gil Sepulveda <jhongil96@gmail.com>
2017-10-12 00:07:44 -0500

    "Diseño mejorado de la sección de veterinarias"

--- src/pages/veterinaries/veterinaries.ts
+++ src/pages/veterinaries/veterinaries.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
@@ -10,17 +10,19 @@ export class VeterinariesPage {
 
   vets: Array<{title: string, dir: string, distance: number}>
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public element: ElementRef) {
     this.vets = [
       { title: "Carmenchis SA", dir: "Calle 30a #75-34", distance: 0.4 },
       { title: "Peluditos Lindos", dir: "Carrera 75 #32-38", distance: 1 },
       { title: "Gatico Cariñosito", dir: "Calle 33 #25-14", distance: 1.3 },
       { title: "Clinica Veterinaria Aguapanela", dir: "Calle 80 #79-84", distance: 4 },
+      { title: "Sacamoco Manuela Careverga", dir: "Calle Popó #79-84", distance: 7 },
+      { title: "Soy el niño más chiquito, soy el niño más bonito, más pendejito", dir: "Calle Popó #79-84", distance: 7 },
     ]
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad VeterinariesPage');
   }
 
 }
